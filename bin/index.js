b5711d8f4cb67ca4c7d94914ab7fac4e17ee0564
Fozilbek <fl.raimberdiyev@gmail.com>
2024-09-23 10:06:45 +0500

    added hashbang /bin/index.js

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,4 @@
-// bin/index.js
-
-// #!/usr/bin/env node
-
+#!/usr/bin/env node
 const { program } = require('../lib/cli'); // Ensure this matches your export in cli.js
 
 program.parse(process.argv);
